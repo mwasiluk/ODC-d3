afda072d3309933dea97df18eb79c5a59e7c318d
mwasiluk <m.wasiluk89@gmail.com>
2016-07-13 20:48:27 +0200

    regression demo

---
+++ regression/demo.js
@@ -0,0 +1,52 @@
+var confFlowers = {
+    // width: 500,
+    // height: 500,
+    guides: true,
+    dot:{
+        // color: function(d) { return d[0]*d[1] }
+    },
+    x:{
+        key: 'petal length',
+        label: 'petal length'
+    },
+    y:{
+        key: 'petal width',
+        label: 'petal width'
+    },
+    groups:{
+        key: 'species',
+        label: "species"
+    },
+    mainRegression: false,
+    groupRegression: true,
+    confidence:{
+        level: 0.95
+    }
+
+
+};
+
+
+var confArray={
+
+};
+
+var dataArray = [
+    [1,2, 1],
+    [2,3, 2],
+    [3,4, 1],
+    [6,4, 3],
+    [11,3, 1],
+    [1,3.5, 4],
+    [7,4, 1],
+    [5,4, 2]
+
+];
+var plot;
+var conf = confFlowers;
+
+d3.csv("../data/flowers.csv", function(error, data) {
+    console.log(data);
+    plot = new ODCD3.Regression("#plot", data, conf);
+
+});
